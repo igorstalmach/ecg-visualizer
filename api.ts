801a59db5d1f827784a62b9dca50d5601c4c4057
Igor Stalmach <igorstalmach@outlook.com>
2025-05-14 06:33:01 +0200

    Update API

--- api.ts
+++ api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://138.199.227.140:8000';
+const API_URL = 'https://ekg-assistant.rokosz.win';
 
 export const ECG_WFDB_FILES_PARSE_URL = `${API_URL}/ekg/signal`;
 export const ECG_IMAGE_PARSE_URL = `${API_URL}/ekg/image`;
